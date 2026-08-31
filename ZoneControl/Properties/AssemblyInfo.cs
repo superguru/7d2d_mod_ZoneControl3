8963f4b9a64dec82a686a91cc10a2a6b13a1654f
superguru <superguru@users.noreply.github.com>
2026-09-01 01:52:41 +1200

    Update AssemblyInfo.cs

--- ZoneControl/Properties/AssemblyInfo.cs
+++ ZoneControl/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.0.*")]
-[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyVersion("3.2.0.*")]
+[assembly: AssemblyFileVersion("3.2.0")]
