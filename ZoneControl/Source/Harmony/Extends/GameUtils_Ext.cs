ed8e7587fc89fdbeebb14c04ed6af9b1e850d9b1
superguru <superguru@users.noreply.github.com>
2026-09-14 20:23:48 +1200

    Make patch method private

--- ZoneControl/Source/Harmony/Extends/GameUtils_Ext.cs
+++ ZoneControl/Source/Harmony/Extends/GameUtils_Ext.cs
@@ -8,7 +8,7 @@ internal static class GameUtils_Ext
 {
     [HarmonyPrefix]
     [HarmonyPatch(nameof(GameUtils.CheckForAnyPlayerHome))]
-    public static bool CheckForZoneClaim(World world, Vector3i BoxMin, Vector3i BoxMax, ref GameUtils.EPlayerHomeType __result)
+    private static bool CheckForZoneClaim(World world, Vector3i BoxMin, Vector3i BoxMax, ref GameUtils.EPlayerHomeType __result)
     {
         if (ZoneClaimRegistry.DoesAnyZoneOverlap(BoxMin, BoxMax))
         {
